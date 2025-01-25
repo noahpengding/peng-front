9caaf3aed46a4ee3a8ad01216c43afd4e7964508
Yipeng Ding <dingyipeng@dingyipeng.com>
2025-01-25 15:44:14 -0500

    Feature: Peng-chat interaction (#5)
    Feature: Peng-chat interaction (#5)
    
    Added new command types for chat and homelab interactions.
    - Introduced `Homelabcommand` and `Chatcommand` structs.
    - Updated command publishing logic to handle chat commands.
    - Implemented a function to parse chat commands with operator and file path support.

--- models/command.go
+++ models/command.go
@@ -12,3 +12,16 @@ type CommandResponse struct {
 	ResponseType string `json:"response_type"`
 	Text         string `json:"text"`
 }
+
+type Homelabcommand struct {
+	Type    string      `json:"type" binding:"required"`
+	Message interface{} `json:"message" binding:"required"`
+	App     interface{} `json:"app"`
+}
+
+type Chatcommand struct {
+	Type      string `json:"type" binding:"required"`
+	Operator  string `json:"operator" binding:"required"`
+	File_path string `json:"file_path"`
+	Message   string `json:"message"`
+}
